f3a60dbe4a743c819a0c86f25731e4ac7ccfc1cd
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-05-03 18:29:34 +0300

    added some functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import Navbar from "./components/Navbar";
+import React from "react";
+
+import { useEffect } from "react";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "../src/features/cart/cartSlice";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
   return (
     <main>
       <Navbar />
