0e5eb3676e6d084fa6bced1cd1635360237deead
Ugur-Mert <u.mert.ozderr@gmail.com>
2022-05-04 18:43:37 +0300

    axios update.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,31 @@ import React from "react";
 import { useEffect } from "react";
 import CartContainer from "./components/CartContainer";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "../src/features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "../src/features/cart/cartSlice";
 import Navbar from "./components/Navbar";
 import Modal from "./components/Modal";
 
 function App() {
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.modal);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1> Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <main>
       {isOpen && <Modal />}
